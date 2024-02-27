f0c7d9579b94c832e3d8bc9f585762dc2480fb9e
UMERHARROON2004 <156570333+UMERHARROON2004@users.noreply.github.com>
2024-02-27 19:42:36 +0500

    Add files via upload

---
+++ Password_Generator.py
@@ -0,0 +1,9 @@
+import random
+import string
+password_length = 8         #we can set password length as we need
+Char_val = string.ascii_letters + string.digits + string.punctuation      #we ca also use only 1 or 2 or 3 string it depend on us
+password =""
+for i in range(password_length):
+    password += random.choice(Char_val)
+print("Your randomly generated password:\n",password)
+
